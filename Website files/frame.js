0f5e27cd2633d1c3d332c80a05d68220737bdf06
root <root@localhost>
2016-01-21 13:19:41 +0000

    Remove clear() for testing

--- Website files/frame.js
+++ Website files/frame.js
@@ -24,7 +24,7 @@ $('body').mousedown(function (event) {
 UpdateList = function () {
     userinput = $('#menuinput').val();
     items = NodeSelect(userinput);
-    $('#menubuttonsdiv').empty();
+    #$('#menubuttonsdiv').empty();
     $('#menubuttonsdiv').append(items.length);
 }
 
